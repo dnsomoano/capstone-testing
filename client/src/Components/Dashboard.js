de9c6579f532ecc1deaa9ef3320ae10e08703606
Daniel N Somoano <dnsomoano@gmail.com>
2018-10-12 00:17:47 -0400

    Fixed missing marker, displaying events not working.

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "../Stylings/Dashboard.css";
-import { Link } from "react-router-dom";
-// import { geocoder, geocode } from "geocoder";
-// import NodeGeocoder from "node-geocoder";
+// import { fetchingPosition, getCurrentPosition } from "react-geolocation";
+// import { Link } from "react-router-dom";
+// import { Geocoder, Providers } from "node-geocoder";
 import {
   // FeatureGroup,
   Map,
@@ -20,44 +20,28 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //   Date: moment(),
       startDate: moment(),
       data: [],
       id: 0,
       NewLat: 0,
       NewLong: 0,
-      zoom: 13,
+      zoom: 11,
       latitude: 27.964157, // default to Tampa, FL
       longitude: -82.452606, // with these coordinates
       name: "",
       address: "",
-      // newPosition: {
-      lat2: 0,
-      long2: 0
-      // }
-      // isFinished: false,
-      // options: {
-      //   Providers: "openstreetmaps",
-      //   httpAdapter: "https",
-      //   formatter: null
-      // }
+      markers: [],
+      authed: {
+        isLoggedIn: false
+      }
     };
     this.handleChange = this.handleChange.bind(this);
-    // this.getLocation(null); // initiate GPS position call
-  }
-
-  componentDidMount() {
-    console.log({ props: this.props });
   }
 
   getLatest = () => {
     const PROFILE_URL = "https://localhost:5001/api/events";
 
-    fetch(PROFILE_URL, {
-      headers: {
-        // mode: "no-cors"
-      }
-    })
+    fetch(PROFILE_URL)
       .then(resp => resp.json())
       .then(eventsData => {
         this.setState({
@@ -67,29 +51,36 @@ class Dashboard extends Component {
       });
   };
 
-  // Post request
   handleSubmit = e => {
     e.preventDefault();
+    if (this.state.address) {
+      this.getGeo(this.state.address);
+    } else {
+      this.getLocation(); // initiate GPS position call
+    }
     fetch("https://localhost:5001/api/events", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
-        mode: "no-cors"
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         EventName: this.state.name,
-        EventAddress: this.state.address
+        EventAddress: this.state.address,
+        EventLatitude: this.state.latitude,
+        EventLongitude: this.state.longitude
       })
     })
-      .then(resp => resp.json())
+      .then(resp => {
+        console.log("got back");
+        return resp.json();
+      })
       .then(_ => {
-        if (this.state.address) {
-          this.getGeo();
-        }
+        console.log({ _ });
         this.getLatest();
       });
   };
 
+  // Changes values of the state targetting name
   handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value
@@ -103,7 +94,21 @@ class Dashboard extends Component {
     });
   }
 
-  getLocation = e => {
+  // converts address to a latitude and longitude
+  getGeo = location => {
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address= + ${location} + &key=AIzaSyA5-V3BEWeNq_lasnMAL8Bip0_bbvSr03U`
+    )
+      .then(resp => resp.json())
+      .then(data => {
+        const { lat, lng } = data.results[0].geometry.location;
+        const points = [lat, lng];
+        this.setState({ markers: [...this.state.markers, points] });
+      });
+  };
+
+  // gets location based on geolocation
+  getLocation = () => {
     navigator.geolocation.getCurrentPosition(position => {
       this.setState({
         latitude: position.coords.latitude,
@@ -112,101 +117,59 @@ class Dashboard extends Component {
     });
   };
 
-  //   getGeocode = location => {
-  //     let options = {
-  //       provider: "openstreetmap"
-  //     };
-  //     let geocoder = NodeGeocoder(options);
-  //     geocoder.geocode(location, function(err, res) {
-  //       console.log(res);
-  //       console.log(typeof res);
-  //     });
-  //   };
-
-  getGeo = () => {
-    fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?address= + ${
-        this.state.address
-      } + &key=AIzaSyA5-V3BEWeNq_lasnMAL8Bip0_bbvSr03U`
-    )
-      .then(resp => resp.json())
-      .then(data => {
-        console.log(data.results[0].geometry.location);
-        console.log(data.results[0].geometry.location.lat);
-        console.log(typeof data.results[0].geometry.location);
-        this.setState({
-          // newPosition: {
-          lat2: data.results[0].geometry.location.lat,
-          long2: data.results[0].geometry.location.lng
-          // }
-        });
-      });
-  };
-
   render() {
+    let button = <section>Loading...</section>;
     const positionOnMap = [this.state.latitude, this.state.longitude];
-    const positionOnMap2 = [this.state.lat2, this.state.long2];
-    console.log(positionOnMap2);
-    console.log(positionOnMap);
-    // if (!this.state.newPosition) {
-    //   this.setState({});
-    // }
+
+    if (this.state.authed.isLoggedIn) {
+      button = (
+        <section className="dashboard">
+          <span className="account-info">
+            <section className="account-box">
+              <img
+                className="profile-picture"
+                src={this.state.authed.profile.picture}
+                alt={this.state.authed.profile.nickname}
+              />
+              <section className="account-name">
+                <section className="greeting">
+                  Welcome {this.state.authed.profile.name}!
+                  <button onClick={this.logout} className="logout-button">
+                    not you?
+                  </button>
+                </section>
+              </section>
+            </section>
+          </span>
+        </section>
+      );
+    }
     return (
       <div className="dashboard-body">
+        {button}
         <section>
-          <form onSubmit={this.handleSubmit}>
-            <section className="row">
-              <header className="field-header">Name of Event:</header>
-              <input
-                type="text"
-                name="name"
-                placeholder="name of event"
-                value={this.state.name}
-                onChange={this.handleChange}
-              />
-            </section>
-            <section className="row">
-              <header className="field-header">Location:</header>
-              <input
-                type="text"
-                name="address"
-                placeholder="location of event"
-                value={this.state.address}
-                onChange={this.handleChange}
-              />
-            </section>
-            <section className="row">
-              <header className="field-header">Time Start:</header>
-              <input type="text" placeholder="0:00" />
-            </section>
-            <section className="row">
-              <header className="field-header">Time End:</header>
-              <input type="text" placeholder="0:00" />
-            </section>
-            <button>Submit</button>
-          </form>
+          {this.state.data.map((event, i) => {
+            return <section key={i}>{event.EventName}</section>;
+          })}
         </section>
         <section className="map-container">
           <Map
             center={positionOnMap}
             zoom={this.state.zoom}
             className="map-styling"
-            onClick={this.getGeo}
           >
             <TileLayer
               attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <Marker position={positionOnMap}>
-              <Popup>
-                Coordinates: [{this.state.latitude},{this.state.longitude}]
-              </Popup>
+              <Popup>Coordinates: {positionOnMap}</Popup>
             </Marker>
-            <Marker position={positionOnMap2}>
-              <Popup>
-                Coordinates: [{this.state.lat2},{this.state.long2}]
-              </Popup>
-            </Marker>
+            {this.state.markers.map((mark, i) => (
+              <Marker key={i} position={mark}>
+                <Popup>Coordinates: {mark}</Popup>
+              </Marker>
+            ))}
           </Map>
           <section className="side-bar">
             <DatePicker
@@ -215,35 +178,38 @@ class Dashboard extends Component {
               selected={this.state.startDate}
               onChange={this.handleDateChange}
             />
-            <Link to="/new_event">
-              <button className="add-event-button">Add Event</button>
-            </Link>
-            <section>
-              <header className="event-list">Events of the Day</header>
-              <section>
-                {this.state.data.map((event, i) => {
-                  return (
-                    <section className="single-event" key={i}>
-                      <button className="event-button">
-                        {event.EventName}
-                      </button>
-                    </section>
-                  );
-                })}
-              </section>
+            <section className="foo">
+              <form className="form-container" onSubmit={this.handleSubmit}>
+                <section className="row">
+                  <header className="field-header">Name of Event:</header>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="name of event"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                  />
+                </section>
+                <section className="row">
+                  <header className="field-header">Location:</header>
+                  <input
+                    type="text"
+                    name="address"
+                    placeholder="location of event"
+                    value={this.state.address}
+                    onChange={this.handleChange}
+                  />
+                  <section className="button-container">
+                    <button className="submit-button">Submit</button>
+                  </section>
+                </section>
+              </form>
             </section>
           </section>
         </section>
-        <section />
       </div>
     );
   }
 }
 
 export default Dashboard;
-// geolocated({
-//   positionOptions: {
-//     enableHighAccuracy: false
-//   },
-//   userDecisionTimeout: 5000
-// })(Dashboard);
